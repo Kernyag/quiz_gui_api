9fef0ffc1bb30bad4b9212978069d48335a7b605
Gyorgy Kernya <kernyag@gmail.com>
2024-02-16 20:52:30 +0100

    Adding question check and feedback

--- ui.py
+++ ui.py
@@ -11,7 +11,7 @@ class GUI():
         self.window.title = ("QUIZ")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
         #Label
-        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label = Label(text=f"Score: {self.quiz.score}/{self.quiz.question_number}", fg="white", bg=THEME_COLOR)
         self.score_label.grid(row=0, column=1)
         #Canvas
         self.canvas = Canvas(width=300, height=250, bg="white")
@@ -20,9 +20,9 @@ class GUI():
         #Buttons
         correct_img = PhotoImage(file="images/true.png")
         wrong_img = PhotoImage(file="images/false.png")
-        self.true_but = Button(image=correct_img, highlightthickness=0)
+        self.true_but = Button(image=correct_img, highlightthickness=0, command=self.true_button_pressed)
         self.true_but.grid(row=2, column=0)
-        self.false_but = Button(image=wrong_img, highlightthickness=0)
+        self.false_but = Button(image=wrong_img, highlightthickness=0, command=self.false_button_pressed)
         self.false_but.grid(row=2, column=1)
 
         self.get_next_question()
@@ -30,5 +30,28 @@ class GUI():
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.text, text=q_text)
+            self.score_label.config(text=f"Score: {self.quiz.score}/{self.quiz.question_number}", fg="white", bg=THEME_COLOR)
+        else:
+            self.canvas.itemconfig(self.text, text=f"No more question left! Your score is: {self.quiz.score}/{self.quiz.question_number}")
+            self.false_but.config(state="disable")
+            self.true_but.config(state="disable")
+        
+
+    def true_button_pressed(self):
+        is_true = self.quiz.check_answer("true")
+        self.give_feedback(is_true)
+
+    def false_button_pressed(self):
+        is_true = self.quiz.check_answer("false")
+        self.give_feedback(is_true)
+
+    def give_feedback(self, correct):
+        if correct:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
